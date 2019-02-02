d536c95932aa89d0fb304cec80eac5994b09285d
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-02 13:51:54 +0100

    create restaurants path to get a list of restaurants

--- server.js
+++ server.js
@@ -49,6 +49,47 @@ app.get('/' + foodsPath, async (req, res) => {
     }
 });
 
+//Fetch restaurants instances
+app.get('/' + restaurantPath, async (req, res) => {
+    try {
+        let restaurantReference = firebase.database().ref("/" + restaurantPath + "/");
+        if (req.query.food) {
+            restaurantReference = restaurantReference.orderByChild("food").equalTo(req.query.food);
+        }
+        if (req.query.equalTo) {
+            restaurantReference = restaurantReference.orderByChild("name").equalTo(req.query.name);
+        }
+        //Attach an asynchronous callback to read the data
+        restaurantReference.on("value",
+        function (snapshot) {
+            let restaurants = [];
+            snapshot.forEach(function (item) {
+                if (req.query.name) {
+                    if (req.query.equalTo) {
+                        restaurants.push(Object.keys(snapshot.val())[0]);
+                    } else {
+                        if (item.val().name.toLowerCase().includes(req.query.name.toLowerCase())) {
+                            restaurants.push(item.val());
+                        }
+                    }
+                } else {
+                    restaurants.push(item.val());
+                }
+            });
+            res.json(restaurants);
+            restaurantReference.off("value");
+        },
+        function (errorObject) {
+            res.type('application/json')
+                .send(printResponse(false, "The read failed: " + errorObject.code));
+        });
+    } catch (err) {
+        res.status(500)
+            .type('application/json')
+            .send(printResponse(false, err));
+    }
+});
+
 
 
 
