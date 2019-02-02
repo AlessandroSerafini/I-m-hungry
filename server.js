d05c00c97ef9ad2c57827f2ebdf8108b240988de
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-02 13:50:02 +0100

    create food path to get a list of foods

--- server.js
+++ server.js
@@ -24,6 +24,31 @@ app.get('/', function (req, res) {
         .send(printResponse(true, "Navigate to /" + restaurantPath + "/:id to get info about it"));
 });
 
+//Fetch foods instances
+app.get('/' + foodsPath, async (req, res) => {
+    try {
+        let foodsReference = firebase.database().ref("/" + foodsPath + "/");
+        //Attach an asynchronous callback to read the data
+        foodsReference.on("value",
+        function (snapshot) {
+            let foods = [];
+            snapshot.forEach(function (item) {
+                foods.push(item.val());
+            });
+            res.json(foods);
+            foodsReference.off("value");
+        },
+        function (errorObject) {
+            res.type('application/json')
+                .send(printResponse(false, "The read failed: " + errorObject.code));
+        });
+    } catch (err) {
+        res.status(500)
+        .type('application/json')
+        .send(printResponse(false, err));
+    }
+});
+
 
 
 
