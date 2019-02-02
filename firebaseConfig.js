813b3a7a4858d7dab927c5c73b7d4a8bb2cfb831
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-02 13:11:31 +0100

    add firebaseConfig file

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebase = require('firebase');
+var config = {
+    apiKey: "AIzaSyAvQdZzeLiuVGoFcbW8gNZq7lhgi6VPuO8",
+    authDomain: "uniurb.firebaseapp.com",
+    databaseURL: "https://uniurb.firebaseio.com",
+    projectId: "uniurb",
+    storageBucket: "uniurb.appspot.com",
+    messagingSenderId: "847042412080"
+};
+firebase.initializeApp(config);
