9fe13e94b39047c7516abc8ad13811c5982c9b4d
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-02 14:14:42 +0100

    implemented http request

--- webService.js
+++ webService.js
@@ -12,7 +12,29 @@ module.exports = {
                     'Content-Type': 'application/json'
                 }
             };
+        let req = https.request(data, function (res) {
+            let output = '';
+            res.setEncoding('utf8');
 
+            res.on('data', function (chunk) {
+                output += chunk;
+            });
+
+            res.on('end', function () {
+                try {
+
+                } catch (err) {
+
+                }
+
+            });
+        });
+        req.on('error', function (err) {
+
+        });
+        if ('body' in options) {
+            req.write(options.body);
+        }
         req.end();
     });
     }
