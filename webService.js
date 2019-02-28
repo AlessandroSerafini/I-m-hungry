2c9b72da73408f7cd79cf4f0d41830825301c153
AleSerafini <alessandro@mr-apps.com>
2019-02-28 09:25:01 +0100

    formatted code

--- webService.js
+++ webService.js
@@ -14,40 +14,42 @@ module.exports = {
                     'Authorization': authConfig.secretCookieValue
                 }
             };
-        let req = https.request(data, function (res) {
-            let output = '';
-            res.setEncoding('utf8');
+            let req = https.request(data, function (res) {
+                let output = '';
+                res.setEncoding('utf8');
 
-            res.on('data', function (chunk) {
-                output += chunk;
+                res.on('data', function (chunk) {
+                    output += chunk;
+                });
+
+                res.on('end', function () {
+                    try {
+                        let obj = JSON.parse(output);
+                        resolve({
+                            statusCode: res.statusCode,
+                            obj: obj
+                        });
+                    } catch (err) {
+                        reject({
+                            statusCode: 500,
+                            obj: err.message
+                        });
+                    }
+
+                });
             });
-
-            res.on('end', function () {
-                try {
-                    let obj = JSON.parse(output);
-                    resolve({
-                        statusCode: res.statusCode,
-                        obj: obj
-                    });
-                } catch (err) {
-                    reject({
-                        statusCode: 500,
-                        obj: err.message
-                    });
-                }
-
+            req.on('error', function (err) {
+                reject({
+                    statusCode: 500,
+                    obj: err.message
+                });
             });
+            if ('body' in options) {
+                req.write(options.body);
+            }
+            req.end();
         });
-        req.on('error', function (err) {
-            reject({
-                statusCode: 500,
-                obj: err.message
-            });
-        });
-        if ('body' in options) {
-            req.write(options.body);
-        }
-        req.end();
-    });
     }
 };
+
+
